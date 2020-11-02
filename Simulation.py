6f9a5ada3f83398b9ca3d1c72b481e7e0f548168
Maiwennn <45673194+Maiwennn@users.noreply.github.com>
2020-11-02 17:57:30 +0100

    Add files via upload
    read/compute data and sort the two lists

---
+++ Simulation.py
@@ -0,0 +1,28 @@
+import numpy as np
+import pandas as pd
+
+fileName = "DonneesControle.txt"
+
+if __name__ == "__main__":
+
+   data = pd.read_csv(fileName, sep=" ", header=None)
+   # data.columns = ["loiUne", "loiDeux", "rien"]
+   print(data.shape)
+
+   inter_arrivee = []
+   duree_controle = []
+
+   for i in range(0, 74):
+      res = data[0][i+1] - data[0][i]
+      inter_arrivee.append(res)
+      duree_controle.append(data[1][i])
+
+   duree_controle.append(data[1][74])
+
+   print(len(inter_arrivee))
+   print(type(inter_arrivee))
+   print(type(duree_controle))
+   inter_arrivee.sort()
+   duree_controle.sort()
+   print(inter_arrivee)
+   print(duree_controle)
