4777ed350f75eaf878a7fbf3132beda833f114a9
yoann <yoann.dupas@outlook.fr>
2020-11-27 17:02:34 +0100

    avancement seance 3

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import numpy.random as npr
+import numpy as np
 import matplotlib.pyplot as plt
 
 
@@ -20,6 +21,8 @@ class Simulateur:
         self.y_qr = list()
         self.y_aire_qc = list()
         self.y_nb_bus = list()
+        self.x_time_sd = list()
+        self.y_qc_sd = list()
 
         # ressource
         self.nb_bus = 0
@@ -36,6 +39,8 @@ class Simulateur:
         self.aire_qc = 0
         self.aire_qr = 0
         self.aire_br = 0
+        self.taille_moyenne_file_c = 0
+        self.taille_moyenne_file_r = 0
 
         # paramètre
         self.param_reparation = 0.3
@@ -64,7 +69,8 @@ class Simulateur:
 
             # Execution de l'évènement
             couple_evenement(self)
-            self.historique.append((couple_evenement.__str__().split(" ")[1], couple_date, self.nu_bus_traite_control, self.bc, self.br))
+            self.historique.append(
+                (couple_evenement.__str__().split(" ")[1], couple_date, self.nu_bus_traite_control, self.bc, self.br))
 
         print("Fin du simulateur")
 
@@ -88,8 +94,15 @@ class Simulateur:
         self.y_br.append(self.br)
         self.y_qc.append(self.qc)
         self.y_qr.append(self.qr)
-        self.y_aire_qc.append(self.aire_qc/(self.nb_bus+0.00001))
+        self.y_aire_qc.append(self.aire_qc / (self.nb_bus + 0.00001))
         self.y_nb_bus.append(self.nb_bus)
+
+        if len(self.x_time_sd) > 0 and self.x_time_sd[-1] == date_simulation:
+            self.x_time_sd.pop()
+            self.y_qc_sd.pop()
+
+        self.x_time_sd.append(date_simulation)
+        self.y_qc_sd.append(self.qc)
 
 
 def arrivee_bus(simulateur):
@@ -158,15 +171,55 @@ def fin_simulation(simulateur):
         simulateur.attente_moyen_av_controle = simulateur.aire_qc / simulateur.nb_bus
         simulateur.attente_moyen_ap_controle = simulateur.aire_qr / simulateur.nb_bus_r
         simulateur.utilisation_moyen_centre_reparation = simulateur.aire_br / (2 * simulateur.date_fin)
+        simulateur.taille_moyenne_file_c = simulateur.aire_qc / simulateur.date_fin
+        simulateur.taille_moyenne_file_r = simulateur.aire_qr / simulateur.date_fin
     except ZeroDivisionError:
         pass
     print("evenement fin de la simulation")
 
 
 if __name__ == '__main__':
-    simu = Simulateur(debut_simulation, 160*60)
-    for element in simu.historique:
-        print('{:25}'.format(element[0]) + "\t" + '{:20}'.format(str(element[1])) + "\t" + str(element[2]) + "\t" + str(element[3])+ "\t" + str(element[4]))
-    plt.plot(simu.x_time, simu.y_nb_bus)
-    plt.show()
+    moyenne_tps_att_moy_avt_ctrl = 0
+    moyenne_tps_att_moy_avt_rep = 0
+    moyenne_taille_moy_file_c = 0
+    moyenne_taille_moy_file_r = 0
+    moyenne_taux_utilisation_centre_rep = 0
+    list_moyenne = list()
 
+    for i in range(500):
+        simu = Simulateur(debut_simulation, 240*60)
+        moyenne_tps_att_moy_avt_ctrl += simu.attente_moyen_av_controle
+        moyenne_tps_att_moy_avt_rep += simu.attente_moyen_ap_controle
+        moyenne_taille_moy_file_c += simu.taille_moyenne_file_c
+        moyenne_taille_moy_file_r += simu.taille_moyenne_file_r
+        moyenne_taux_utilisation_centre_rep += simu.utilisation_moyen_centre_reparation
+        list_moyenne.append(simu.attente_moyen_av_controle/500)
+
+    print("list moyenne")
+    print(np.std(list_moyenne))
+    moyenne_tps_att_moy_avt_ctrl /= 500
+    moyenne_tps_att_moy_avt_rep /= 500
+    moyenne_taille_moy_file_c /= 500
+    moyenne_taille_moy_file_r /= 500
+    moyenne_taux_utilisation_centre_rep /= 500
+
+    print("moyenne_tps_att_moy_avt_ctrl")
+    print(moyenne_tps_att_moy_avt_ctrl/60)
+    print("moyenne_tps_att_moy_avt_rep")
+    print(moyenne_tps_att_moy_avt_rep/60)
+    print("moyenne_taille_moy_file_c")
+    print(moyenne_taille_moy_file_c)
+    print("moyenne_taille_moy_file_r")
+    print(moyenne_taille_moy_file_r)
+    print("moyenne_taux_utilisation_centre_rep")
+    print(moyenne_taux_utilisation_centre_rep)
+
+    # simu = Simulateur(debut_simulation, 240 * 60)
+    # plt.plot(simu.x_time_sd, simu.y_qc_sd)
+    # plt.show()
+    # plt.plot(simu.x_time, simu.y_aire_qc)
+    # plt.show()
+    # print(simu.nb_bus)
+    # print(simu.nb_bus_r)
+    # print(simu.x_time_sd)
+    # print(simu.y_qc_sd)
